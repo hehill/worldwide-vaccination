64f98bef1515b3dc9cb88efb16c9bf4bbace9e29
Hannah Hill <you@example.com>
2021-11-22 14:17:19 -0500

    made basic continent graphs

--- scatter/plot.js
+++ scatter/plot.js
@@ -1,17 +1,84 @@
 var data = DATA2;
+console.log(data[0])
 
-// let trace1 = {
-//   x: date,
-//   y: yData
-// };
+function checkContinent() {    
+  if(document.getElementById('asia').checked) {   
+    selected = document.getElementById('asia').value;
+    return selected
+  }   
+  else if(document.getElementById('europe').checked) {   
+    selected = document.getElementById('europe').value;
+    return selected
+  }   
+  else if(document.getElementById('africa').checked) {   
+    selected = document.getElementById('africa').value;
+    return selected
+  }   
+  else if(document.getElementById('north_am').checked) {   
+    selected = document.getElementById('north_am').value;
+    return selected
+  }  
+  else if(document.getElementById('south_am').checked) {   
+    selected = document.getElementById('south_am').value;
+    return selected
+  }
+  else if(document.getElementById('oceania').checked) {   
+    selected = document.getElementById('oceania').value;
+    return selected
+  }  
+  else {
+    return "Asia"
+  }
+}   
 
-// let data = [trace1];
+function makeScatter() {
+var continent = checkContinent();
+const date_list =[];
+const total_vaccination_list =[];
+const total_cases_list =[];
 
-// let layout = {
-//   title: "A Plotly plot"
-// };
+data.forEach(function(value, i) {
+    if (value.country_name == continent) {
+        date_list[i] = value.date;
+        total_vaccination_list[i] = value.total_vaccinations_per_hundred
+        total_cases_list[i] = value.total_cases_per_million
+    }
+});
 
-// Plotly.newPlot("plot", data, layout);
+console.log(date_list)
+console.log(total_vaccination_list)
+
+let trace1 = {
+  x: date_list,
+  y: total_vaccination_list,
+  type: 'scatter',
+  name: 'Total Vaccinations per hundred',
+};
+
+let trace2 = {
+  x: date_list,
+  y: total_cases_list,
+  yaxis: 'y2',
+  type: 'scatter',
+  name: 'Total Cases per million',
+};
+
+let plot_data = [trace1,trace2];
+
+let layout = {
+  title: checkContinent(),
+  yaxis: {title: "Total Vaccinations"},
+  yaxis2: {title: "Total Cases",
+           overlaying: "y",
+           side: "right"},
+};
+
+Plotly.newPlot("scatter", plot_data, layout);
+}
+
+d3.selectAll('input[name = scatter]').on("change", makeScatter);
+
+makeScatter();
 
 // var rawDataURL = 'http://cors.io/?https://github.com/hehill/worldwide-vaccination/blob/main/data/owid-covid-data.csv';
 // var xField = 'date';
